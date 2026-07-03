dd7aeb0fccfe24a569f349de8fc8aeb3e8535bf9
Filip Zynwala <filipzynwala@gmail.com>
2026-07-03 18:36:14 +0200

    small fixes

--- src/Card.js
+++ src/Card.js
@@ -10,7 +10,9 @@ export default function Card({ data }) {
     return (
         <>
             <div className="w-80 my-4 p-4">
-                <div className="text-2xl font-semibold mt-5 mb-2 underline">{data.Title}</div>
+                <button onClick={() => setIsShowMore(!isShowMore)}>
+                    <div className="text-2xl font-semibold mt-5 mb-2 underline">{data.Title}</div>
+                </button>
                 {/* <img
                     src="https://upload.wikimedia.org/wikipedia/commons/5/52/Nabo%C5%BCe%C5%84stwo_majowe_%28ul._P%C5%82owiecka%2C_Sanok%2C_2016%29.jpg"
                     alt="mojowka"
@@ -20,7 +22,7 @@ export default function Card({ data }) {
                 <div className={`prose mt-5 leading-6 ${isShowMore ? '' : 'line-clamp-2'} overflow-hidden`}>
                     <BlocksRenderer content={data.content} />
                 </div>
-                <button onClick={() => setIsShowMore(!isShowMore)}>
+                <button className="underline" onClick={() => setIsShowMore(!isShowMore)}>
                     {isShowMore ? 'Pokaż mniej' : 'Pokaż więcej'}
                 </button>
             </div>
